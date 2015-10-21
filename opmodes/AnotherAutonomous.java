c8499c2f6a05098e3482463613437b3e17155562
dsadaoka19 <dsadaoka19@punahou.edu>
2015-10-20 16:07:24 -1000

    Working Auto
    Working Auto

--- opmodes/AnotherAutonomous.java
+++ opmodes/AnotherAutonomous.java
@@ -32,7 +32,7 @@ public class AnotherAutonomous extends OpMode {
                 motor1.setTargetPosition(1120);
                 motor1.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
-                if (motor1.getCurrentPosition() < 1125 || motor1.getCurrentPosition() > 1115) {
+                if (motor1.getCurrentPosition() < 1125 && motor1.getCurrentPosition() > 1115) {
                     motor1.setPower(0.0);
                     x = 1;
                     motor1.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
@@ -43,7 +43,7 @@ public class AnotherAutonomous extends OpMode {
                 motor1.setTargetPosition(2400);
                 motor1.setChannelMode(DcMotorController.RunMode.RUN_TO_POSITION);
 
-                if (motor1.getCurrentPosition() < 2405 || motor1.getCurrentPosition() > 2395) {
+                if (motor1.getCurrentPosition() < 2405 && motor1.getCurrentPosition() > 2395) {
                     motor1.setPower(0.0);
                     x = 2;
                     motor1.setChannelMode(DcMotorController.RunMode.RESET_ENCODERS);
@@ -51,7 +51,7 @@ public class AnotherAutonomous extends OpMode {
         }
     }
     @Override
-        public void stop(){
+    public void stop(){
 
     }
 
